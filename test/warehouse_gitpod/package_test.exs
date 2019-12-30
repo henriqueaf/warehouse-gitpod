0c8553885010ba3cdfae43a886f86cbf07e736dd
Henrique Almeida <henrique_af1@msn.com>
2019-12-30 16:03:19 +0000

    Create Package tests

--- test/warehouse_gitpod/package_test.exs
+++ test/warehouse_gitpod/package_test.exs
@@ -8,4 +8,20 @@ defmodule WarehouseGitpod.PackageTest do
       assert %Package{id: _id, contents: 'Some content'} = Package.new('Some content')
     end
   end
+  
+  describe "random()" do
+    test "returns a new package stuct with random content" do
+      assert %Package{id: _id, contents: random_content} = Package.random()
+      assert Enum.member?(["Bat", "Ball", "Book", "Broom"], random_content)
+    end
+  end
+  
+  describe "random_batch(n)" do
+    test "returns n number of packages stucts with random contents" do
+      assert [
+        %Package{id: _id1, contents: _content1},
+        %Package{id: _id2, contents: _content2}
+      ] = Package.random_batch(2)
+    end
+  end
 end
